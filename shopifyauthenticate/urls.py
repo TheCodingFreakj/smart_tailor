18a7f182e1ef7f82a85d8bda89c3bcce0e811108
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-11-17 23:21:54 +0530

    init

--- shopifyauthenticate/urls.py
+++ shopifyauthenticate/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views, ShopifyInstallView, ShopifyCallbackView
+from .views import ShopifyInstallView, ShopifyCallbackView, shopify_callback
 
 urlpatterns = [
-    path('auth/callback/', views.shopify_callback, name='shopify_callback'),
+    path('auth/callback/', shopify_callback, name='shopify_callback'),
     path("shopify/install/", ShopifyInstallView.as_view(), name="shopify-install"),
     path("shopify/callback/", ShopifyCallbackView.as_view(), name="shopify-callback"),
 ]
