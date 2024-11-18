22f3a1b7e870bf6f4453b6f00e7fa0b01c4ebfb4
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-11-18 20:03:26 +0530

    init

--- shopifyauthenticate/middleware.py
+++ shopifyauthenticate/middleware.py
@@ -9,7 +9,7 @@ class VerifyAppInstallationMiddleware:
         shop_domain = request.GET.get('shop')
         if shop_domain:
             shop = ShopifyStore.objects.filter(shop_name=shop_domain).first()
-            if not shop or not shop.is_installed:
+            if not shop:
                 return redirect("https://smart-tailor-frnt.onrender.com/error")
 
         response = self.get_response(request)
