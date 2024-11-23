1d57001beec2b5f63c245ee5b9b97308ca3fed06
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-11-24 00:50:21 +0530

    init

--- shopifyauthenticate/views.py
+++ shopifyauthenticate/views.py
@@ -119,9 +119,9 @@ def check_installation_status(request):
     lower_bound = now - time_difference
 
     print(shop.updated_at, lower_bound)
+    urls = [url.strip() for url in shop.urlsPassed.split(",")]
 
-
-    if '/shopify/callback/' in shop.urlsPassed.split(",") and '/shopify/install/' in shop.urlsPassed.split(",") and shop.updated_at >= lower_bound:
+    if '/shopify/callback/' in urls and '/shopify/install/' in urls and shop.updated_at >= lower_bound:
         
         # Attempt to update the existing record, only if it exists
         ShopifyStore.objects.filter(id=shop_id).update(
