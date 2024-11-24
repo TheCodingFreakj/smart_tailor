81d6c9453a452e34287ff9eacb03ea94efe69d35
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-11-24 11:36:39 +0530

    init

--- shopifyauthenticate/middleware.py
+++ shopifyauthenticate/middleware.py
@@ -39,31 +39,15 @@ class ShopifyAuthMiddleware(MiddlewareMixin):
                     is_installed="installed"
                 )
 
-                if shop.urlsPassed.split(",")[1] == 'https://admin.shopify.com/':
+                 
+
+                if len(shop.urlsPassed.split(",")) > 1 and shop.urlsPassed.split(",")[1] == 'https://admin.shopify.com/':
                     request.auth = True
                 else:
                     request.auth = False       
             except (json.JSONDecodeError, UnicodeDecodeError) as e:
                 print(f"Error decoding request body: {e}")
- 
 
-    
-        if request.path == '/shopify/callback/':
-            code = request.GET.get('code')
-            request.code = code     
-        if request.path == '/shopify/install/':
-                full_url = request.build_absolute_uri()
-                parsed_url = urlparse(full_url)
-                query_params = parse_qs(parsed_url.query)
-                hmac_value = query_params.get('hmac', [None])[0]
-                hmac_received = request.GET.get('hmac')
-                shopify_hmac = hmac_received if hmac_received else hmac_value
-                ShopifyStore.objects.update_or_create(
-                    shop_name=request.GET.get('shop'),  # Use the shop name as the unique identifier
-                    defaults={'current_hmac': shopify_hmac, "canAsk":True}  # Update the access token
-                )
-                print(f"hmac_received----------->{hmac_received}")  
-                print(f"hmac_value----------->{hmac_value}")
 
     def process_response(self, request, response):
         print("Executing after the view.")
@@ -71,7 +55,7 @@ class ShopifyAuthMiddleware(MiddlewareMixin):
         # Check if the response is a JsonResponse
         if isinstance(response, JsonResponse):
             # Extract the JSON data from the response
-            response_data = response.json()  # This will give you the dictionary
+            response_data = json.loads(response.content)  # This will give you the dictionary
             
             # Check if 'shop' attribute exists in the response
             shop = response_data.get('shop')
@@ -87,29 +71,3 @@ class ShopifyAuthMiddleware(MiddlewareMixin):
 
         # Optionally, modify the response if needed before returning
         return response
-    
-
-# class ShopifyHMACVerificationMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-
-#     def __call__(self, request):
-#         print(f"request.path---->{request.path}")
-#             # Extract the HMAC from the request headers
-#         referer = request.META.get('HTTP_REFERER', 'Unknown')    
-#         print(f"referer------------------>{referer}") 
-#         request.shopify_referer = referer   
-
-#         if request.path == '/shopify/install/':
-#                 full_url = request.build_absolute_uri()
-#                 parsed_url = urlparse(full_url)
-#                 query_params = parse_qs(parsed_url.query)
-#                 hmac_value = query_params.get('hmac', [None])[0]
-#                 hmac_received = request.GET.get('hmac')
-#                 request.shopify_hmac = hmac_received if hmac_received else hmac_value
-#                 print(f"hmac_received----------->{hmac_received}")  
-#                 print(f"hmac_value----------->{hmac_value}")  
-
-#         # If the request is valid, pass it along
-#         response = self.get_response(request)
-#         return response
